58f4690b17ca38d7678588f5f293ab48bfa05fbe
ErdMutter92 <alexis@bleauweb.net>
2014-09-15 00:09:17 -0400

    Checks for invalid urls...

--- InstaSave.py
+++ InstaSave.py
@@ -1,20 +1,32 @@
 import urllib2, sys
 
 def downloadPhoto(url):
-    
-    website = urllib2.urlopen(url)
-    website = website.read()
-    
-    website = website.split('<meta property="og:image" content="')
-    photo = website[1].split('" />')
-    
 
-    photo_name = photo[0].split('/')
-    photo_file = urllib2.urlopen(photo[0])
+    photo_url = url.split('/')
+    
+    if len(photo_url[-1]) == 10:
+        if len(photo_url[-2]) == 1:
+            if photo_url[-3] == 'instagram.com':
+    
+                website = urllib2.urlopen(url)
+                website = website.read()
+    
+                website = website.split('<meta property="og:image" content="')
+                photo = website[1].split('" />')
+    
+    
+                photo_name = photo[0].split('/')
+                photo_file = urllib2.urlopen(photo[0])
+        
+                with open(photo_name[-1], 'wb') as output:
+                    output.write(photo_file.read())
 
-    with open(photo_name[-1], 'wb') as output:
-        output.write(photo_file.read())
-
-    print "Downloaded 1 Photo: ", photo_name[-1]
+                print "Downloaded 1 Photo: ", photo_name[-1]
+            else:
+                print "Invalid URL:", url
+        else:
+            print "Invalid URL:", url
+    else:
+        print "Invalid URL:", url
 
 downloadPhoto(sys.argv[-1])
